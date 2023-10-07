36b70aa26b28aac8db4e340d7a9d5d55a12ff90f
Marlon Peralta <marlonperalta@digital.gob.pe>
2023-10-07 13:41:57 -0500

    bases-const-let

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+// variables y constantes
+
+const nombre = 'Marlon';
+const apellido = 'Peralta';
+
+let valorDado = 5;
+valorDado = 6
+
+console.log(nombre, apellido, valorDado);
+
+if ( true ) {
+    let valorDado = 7;
+    console.log(valorDado)
+}
+
+console.log(valorDado);
